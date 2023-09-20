59de26ee9f6e0dbd4d8e621020c677bbead9f2f2
dariofusco <fdario@hotmail.it>
2023-09-20 17:31:57 +0200

    fix istanza

--- Prodotto.php
+++ Prodotto.php
@@ -8,26 +8,14 @@ class Prodotto extends Categoria
     public $gioco;
     public $cuccia;
 
-    public function __construct($cibo, $gioco, $cuccia)
+    public function __construct($cibo, $gioco, $cuccia, $categoria)
     {
         $this->cibo = $cibo;
         $this->gioco = $gioco;
         $this->cuccia = $cuccia;
+        $this->categoria = $categoria;
     }
-
-    /*public function getHTMLCard()
-    {
-        return "
-        <div>
-        <h2>$this->categoria</h2>
-        <ul>
-            <li>Cibo: $this->cibo</li>
-            <li>Gioco: $this->gioco</li>
-            <li>Cuccia: $this->cuccia</li>
-        </ul>
-        ";
-    }*/
 }
 
-$cani = new Prodotto('Scatoletta', 'Osso', 'Casetta');
-$gatti = new Prodotto('Croccantini', 'Pallina', 'Cuscino');
+$prodotto1 = new Prodotto('Scatoletta', 'Osso', 'Casetta', $cani);
+$prodotto2 = new Prodotto('Croccantini', 'Pallina', 'Cuscino', $gatti);
--- index.php
+++ index.php
@@ -3,19 +3,22 @@
 require_once __DIR__ . '/Prodotto.php';
 require_once __DIR__ . '/Categoria.php';
 
-var_dump($cani);
-var_dump($gatti);
+var_dump($prodotto1);
+var_dump($prodotto2);
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
 
 </body>
+
 </html>
